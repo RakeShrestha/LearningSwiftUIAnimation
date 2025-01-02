8b3a22714d00a9aef4e6e13b48e684c2859b54e6
RakeShrestha <orakeshstha7@gmail.com>
2025-01-02 07:34:38 +0545

    Initial Commit

---
+++ LearningSwiftUIAnimation/LearningSwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIAnimationApp.swift
+//  LearningSwiftUIAnimation
+//
+//  Created by Rakesh Shrestha on 02/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
